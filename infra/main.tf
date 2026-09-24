7ae23ea10dae503f75b9f545d66c71ecc312a39d
Duy <duypersonal512@gmail.com>
2026-09-24 10:36:19 -0500

    feat: init terraform

---
+++ infra/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.60"
+    }
+  }
+
+  required_version = "~> 1.15.8"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
